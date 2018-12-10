ed345be1d1a503a8cdcc4fe89a5ff11df45cc5cd
root <root@openshift-01.localdomain>
2018-12-10 20:09:44 +0800

    fix

--- k8s/create_config_map.sh
+++ k8s/create_config_map.sh
@@ -1,2 +1,2 @@
-kubectl create configmap sidecar-config-b --from-file=envoy-config=../service_b/envoy-config.yaml -n sm
-kubectl create configmap sidecar-config-c --from-file=envoy-config=../service_c/envoy-config.yaml -n sm
+kubectl create configmap sidecar-config-b --from-file=envoy-config=../service_b/envoy-config.yaml -n default
+kubectl create configmap sidecar-config-c --from-file=envoy-config=../service_c/envoy-config.yaml -n default
